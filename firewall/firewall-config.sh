b1e1c197e1df448c57eb47e7783f35d6651bc485
DaniielFlorez <daniels.florezi@uqvirtual.edu.co>
2024-10-01 20:44:10 -0500

    Se creo estructura del firewall

---
+++ firewall/firewall-config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Borrar reglas anteriores
+iptables -F
+
+# Permitir tráfico entrante a puertos específicos (e.g., web-app en el puerto 5000, Nginx en el puerto 80)
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 5000 -j ACCEPT
+
+# Bloquear cualquier otro tráfico no autorizado
+iptables -A INPUT -j DROP
+
+# Permitir tráfico de la red interna de Docker
+iptables -A INPUT -i lo -j ACCEPT
